1e48a354dcd60ea86e93783b7d3eeb87a5f4ef97
Martin Podloucký <podlouckymartin@gmail.com>
2025-11-30 18:02:49 +0100

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Welcome to Chessnut</h1>
+      <p>React 19 + TypeScript + Webpack</p>
+    </div>
+  );
+};
+
+export default App;

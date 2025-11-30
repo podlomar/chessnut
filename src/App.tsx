85e426f996c2e1b332acacffbcea891230be7543
Martin Podloucký <podlouckymartin@gmail.com>
2025-11-30 21:15:26 +0100

    Implement first phase of valid moves detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ChessBoard } from './components/ChessBoard';
-import { readPosition } from './chessnut';
+import { ChessnutDriver, readPosition } from './chessnut';
 import { Chess } from 'chess.js';
 
-const chess = new Chess();
-chess.clear();
-
-const dataEquals = (data1: Uint8Array, data2: Uint8Array) => {
-  for (let i = 0; i < data1.length; i++) {
-    if (data1[i] !== data2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const initMonitoring = async (callback: (chess: Chess) => void) => {
-  const devices = await navigator.hid.requestDevice({
-    filters: [{
-      vendorId: 0x2d80,
-      usagePage: 0xFF00
-    }]
-  });
-
-  const device = devices[0];
-  await device.open();
-  const collection = device.collections.find(
-    c => c.usagePage === 0xFF00
-  );
-
-  if (!collection) {
-    console.error("Could not find usagePage 0xFF00");
-    return;
-  }
-
-  const reportId = 0x21;
-  await device.sendReport(reportId, new Uint8Array([0x01, 0x00]));
-
-  let lastData: Uint8Array | null = null;
-
-  device.addEventListener("inputreport", event => {
-    const { data, reportId } = event;
-
-    if (reportId !== 0x01) {
-      return;
-    }
-
-    const bytes = new Uint8Array(data.buffer);
-    const newData = bytes.slice(1, 33);
-
-    if (lastData && dataEquals(lastData, newData)) {
-      console.log("No changes detected");
-      return;
-    }
-
-    lastData = newData;
-    const chess = readPosition(newData);
-    callback(chess);
-  });
-}
+// const chess = new Chess();
+// console.log("Moves", chess.moves({ verbose: true }));
 
 export const App = () => {
   const [chess, setChess] = useState<Chess | null>(null);
 
   const handleConnect = () => {
-    initMonitoring((newChess) => {
-      console.log("Report", newChess);
-      setChess(newChess);
-    });
+    const driver = new ChessnutDriver(setChess);
+    driver.connect();
   };
 
   return (
--- src/chessnut.ts
+++ src/chessnut.ts
@@ -1,11 +1,11 @@
-import { Chess, Color, PieceSymbol, Square } from 'chess.js';
+import { Chess, Color, Move, PieceSymbol, Square } from 'chess.js';
 
 export interface Piece {
   type: PieceSymbol;
   color: Color;
 }
 
-// '.', 'q', 'k', 'b', 'p', 'n', 'R', 'P', 'r', 'B', 'N', 'Q', 'K'
+console.log("Piece symbols:", new Chess().fen());
 
 const pieces: (Piece | null)[] = [
   null,
@@ -52,5 +52,90 @@ export const readPosition = (data: Uint8Array): Chess => {
     }
   }
 
+  chess.setTurn('w');
   return chess;
+}
+
+const dataEquals = (data1: Uint8Array, data2: Uint8Array) => {
+  for (let i = 0; i < data1.length; i++) {
+    if (data1[i] !== data2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const getPlacement = (fen: string): string => {
+  return fen.split(' ')[0];
+};
+
+export class ChessnutDriver {
+  private currentState: Chess | null = null;
+  private validMoves: Move[] = [];
+  private onNewState: ((chess: Chess) => void);
+  private lastData: Uint8Array | null = null;
+
+  public constructor(onNewState: (chess: Chess) => void) {
+    this.onNewState = onNewState;
+  }
+
+  public async connect() {
+    const devices = await navigator.hid.requestDevice({
+      filters: [{
+        vendorId: 0x2d80,
+        usagePage: 0xFF00
+      }]
+    });
+
+    const device = devices[0];
+    await device.open();
+    const collection = device.collections.find(
+      c => c.usagePage === 0xFF00
+    );
+
+    if (!collection) {
+      console.error("Could not find usagePage 0xFF00");
+      return;
+    }
+
+    const reportId = 0x21;
+    await device.sendReport(reportId, new Uint8Array([0x01, 0x00]));
+
+    device.addEventListener("inputreport", event => {
+      const { data, reportId } = event;
+
+      if (reportId !== 0x01) {
+        return;
+      }
+
+      const bytes = new Uint8Array(data.buffer);
+      const newData = bytes.slice(1, 33);
+
+      if (this.lastData !== null && dataEquals(this.lastData, newData)) {
+        console.log("No changes detected");
+        return;
+      }
+
+      this.lastData = newData;
+      const newState = readPosition(newData);
+      if (this.currentState === null) {
+        this.currentState = newState;
+        this.validMoves = this.currentState.moves({ verbose: true });
+        this.onNewState(newState);
+        return;
+      }
+
+      const placement = getPlacement(newState.fen());
+      for (const move of this.validMoves) {
+        const afterPlacement = getPlacement(move.after);
+        if (afterPlacement === placement) {
+          console.log("Detected move:", move);
+          this.currentState = new Chess(move.after);
+          this.validMoves = this.currentState.moves({ verbose: true });
+          this.onNewState(this.currentState);
+          break;
+        }
+      }
+    });
+  }
 }
